f448ba18b0725ee28828361a71c98708d06cb72d
Amit Gupta <125515651+amitgupta1324@users.noreply.github.com>
2024-05-09 15:54:12 +0530

    Add files via upload

---
+++ #2_remove_element.cpp
@@ -0,0 +1,14 @@
+class solution {
+    public:
+    int removeElement(vector<int>& nums, int val){
+        int k=0;
+        for (int  i = 0; i < nums.size(); i++)
+        {
+        /* code */if(nums[i]!=val){
+            nums[k]=nums[i];
+            k++;
+        }
+        }
+        return k;
+    }
+};
